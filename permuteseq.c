1dc0180ab6f133c075c848cea8cbc051df4c7007
Martin Skovvang Petersen <martin@kviksoft.com>
2022-08-19 22:11:16 +0200

    Update permuteseq.c

--- permuteseq.c
+++ permuteseq.c
@@ -253,7 +253,7 @@ cycle_walking_cipher(int64 minval, int64 maxval, int64 value, uint64 crypt_key, 
                  c;
 	
 	a = b = c = 0x9e3779b9 + (uint32) sizeof(uint32) + 3923095;
-     	/*a += crypt_key & 0xffffffff;*/
+     	a += crypt_key & 0xffffffff;
 	
 	return a;
 
