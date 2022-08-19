794abb50cf61063ad3eed291447cf8d7dc58c1c6
Martin Skovvang Petersen <martin@kviksoft.com>
2022-08-19 22:07:36 +0200

    Update permuteseq.c

--- permuteseq.c
+++ permuteseq.c
@@ -255,7 +255,7 @@ cycle_walking_cipher(int64 minval, int64 maxval, int64 value, uint64 crypt_key, 
 	a = b = c = 0x9e3779b9 + (uint32) sizeof(uint32) + 3923095;
      	a += crypt_key & 0xffffffff;
 	
-	return a;
+	return crypt_key & 0xffffffff;
 
 	/* Scramble the key. This is not strictly necessary, but will
 	   help if the user-supplied key is weak, for instance with only a
