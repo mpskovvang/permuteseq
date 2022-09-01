af2fdcc75f154cd0491c81ff75df882dc8619a24
Martin Skovvang Petersen <martin@kviksoft.com>
2022-09-01 15:03:17 +0200

    Update permuteseq.c

--- permuteseq.c
+++ permuteseq.c
@@ -288,10 +288,10 @@ cycle_walking_cipher(int64 minval, int64 maxval, int64 value, uint64 crypt_key, 
 			   j=(NR-1-i), i.e. we iterate over subkeys in the reverse order. */
 			Ki = crypt_key >> ((hsz* (direction==0 ? i : NR-1-i))&0x3f);
 			Ki += (direction==0 ? i : NR-1-i);
-			return Ki;
 			r2 = (l1 ^ DatumGetUInt32(hash_uint32(r1))
 			         ^ DatumGetUInt32(hash_uint32(Ki))
 			      ) & mask;
+			return r2;
 			l1 = l2;
 			r1 = r2;
 		}
