8b2a12efc3d1c6bc4c027dbc99006bf33f6b3c75
Martin Skovvang Petersen <martin@kviksoft.com>
2022-09-06 20:05:48 +0200

    Update permuteseq.c

--- permuteseq.c
+++ permuteseq.c
@@ -295,7 +295,7 @@ cycle_walking_cipher(int64 minval, int64 maxval, int64 value, uint64 crypt_key, 
 			
 			r2 = (l1 ^ DatumGetUInt32(hash_uint32(r1))
 			         ^ DatumGetUInt32(hash_uint32(Ki))
-			      ); // & mask;
+			      ) & mask;
 			
 			l1 = l2;
 			r1 = r2;
