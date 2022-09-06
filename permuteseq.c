38074543af9b0c4610147c6c85c97bda1fa7c155
Martin Skovvang Petersen <martin@kviksoft.com>
2022-09-06 20:42:51 +0200

    Update permuteseq.c

--- permuteseq.c
+++ permuteseq.c
@@ -264,8 +264,8 @@ cycle_walking_cipher(int64 minval, int64 maxval, int64 value, uint64 crypt_key, 
 	/* Scramble the key. This is not strictly necessary, but will
 	   help if the user-supplied key is weak, for instance with only a
 	   few right-most bits set. */
-	crypt_key = hash_uint32(crypt_key & 0xffffffff) |
-		((uint64)hash_uint32((crypt_key >> 32) & 0xffffffff)) << 32;
+	/*crypt_key = hash_uint32(crypt_key & 0xffffffff) |
+		((uint64)hash_uint32((crypt_key >> 32) & 0xffffffff)) << 32;*/
 	
 	elog(DEBUG1, "permuteseq: crypt_key=%"PRIu64"", crypt_key);
 	
